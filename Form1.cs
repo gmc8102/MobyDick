b179463fcac234f67313b2559e074629bbbff5c0
gmc8102 <gmc8102@gmail.com>
2022-05-24 15:17:25 -0500

    First step, read in the 'stop words' list and put them in a list (I used dictionary, no preticular reason).  This version writes some messages to the Output to verify the input was processed as expected it to be.

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,45 @@ namespace MobyDick
 {
     public partial class MobyDickWordCount : Form
     {
+        Dictionary<string, int> StopWords = new Dictionary<string, int>();
+
         public MobyDickWordCount()
         {
             InitializeComponent();
+            GetStopWords();
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void GetStopWords()
+        {
+            int ctLines = 0;
+            int ctBlanks = 0;
+            int ctDups = 0;
+            foreach (string line in System.IO.File.ReadLines(@"C:\temp\stop-words.txt"))
+            {
+                if (line == "")
+                    ctBlanks++;
+                else
+                {
+                    try
+                    {
+                        StopWords.Add(line, 0);
+                    }
+                    catch (Exception)
+                    {
+                        ctDups++;
+                    }
+                }
+                ctLines++;
+            }
+            System.Console.WriteLine("There are {0} lines.", ctLines);
+            System.Console.WriteLine("There are {0} blanks.", ctBlanks);
+            System.Console.WriteLine("There are {0} duplicates.", ctDups);
+            System.Console.WriteLine("There are {0} entries.", StopWords.Count);
         }
     }
 }
