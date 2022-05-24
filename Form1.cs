051c1976020720cf3be567caf878eb8b39035e08
gmc8102 <gmc8102@gmail.com>
2022-05-24 17:11:11 -0500

    Next step, read in the book (Moby Dick) and put the words into a list (again I used a Dictionary, this time because I have to maintain a count of the number of time the word appears in the text).  And again displaying some numbers in the Output to verify it's working.

--- Form1.cs
+++ Form1.cs
@@ -13,11 +13,13 @@ namespace MobyDick
     public partial class MobyDickWordCount : Form
     {
         Dictionary<string, int> StopWords = new Dictionary<string, int>();
+        Dictionary<string, int> GoWords = new Dictionary<string, int>();
 
         public MobyDickWordCount()
         {
             InitializeComponent();
             GetStopWords();
+            GetGoWords();
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -27,6 +29,7 @@ namespace MobyDick
 
         private void GetStopWords()
         {
+            System.Console.WriteLine("Stop Words...");
             int ctLines = 0;
             int ctBlanks = 0;
             int ctDups = 0;
@@ -51,6 +54,50 @@ namespace MobyDick
             System.Console.WriteLine("There are {0} blanks.", ctBlanks);
             System.Console.WriteLine("There are {0} duplicates.", ctDups);
             System.Console.WriteLine("There are {0} entries.", StopWords.Count);
+        }
+
+        private void GetGoWords()
+        {
+            System.Console.WriteLine("Go Words...");
+            Boolean bProcessing = false;
+            //char[] charsToTrim = { '*', ',', '.', ' ', '\'' };
+            char[] splitchars = { ' ', ',', '.', '-', '—' };   
+            int ctLines = 0;
+            int ctWords = 0;
+            foreach (string line in System.IO.File.ReadLines(@"C:\temp\mobydick.txt"))
+            {
+                if (line != "")
+                {
+                    if (line.Length >= 10)
+                    {
+                        if (line.Substring(0, 10) == "CHAPTER 1.")
+                            bProcessing = true;
+                        if (line.Substring(0, 10) == "End of Pro")
+                            break;
+                    }
+                    if (bProcessing)
+                    {
+                        ctLines++;
+                        string[] words = line.Split(splitchars, StringSplitOptions.RemoveEmptyEntries);  // ;
+                        foreach (var word in words)
+                        { 
+                            // if word is not in the dictionary,
+                            if (!GoWords.ContainsKey(word))
+                                // add it to the dictionary
+                                GoWords.Add(word, 1);
+                            else
+                            {
+                                // update the entry by by adding 1 to the int.
+                                GoWords[word] += 1;
+                            };
+                            ctWords++;
+                        }
+                    }
+                }
+            }
+            System.Console.WriteLine("There are {0} lines.", ctLines);
+            System.Console.WriteLine("There are {0} words.", ctWords);
+            System.Console.WriteLine("There are {0} entries.", GoWords.Count);
         }
     }
 }
